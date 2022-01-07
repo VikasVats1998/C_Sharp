1f7b409ec1c1aa4212184a5c85cd1a65b2262162
VikasVats1998 <vvats888@gmail.com>
2022-01-07 07:49:21 +0530

    variable added

---
+++ variable.cs
@@ -0,0 +1,20 @@
+using System;
+namespace variable
+{
+    class variable
+    {
+        public static void Main(string[] args)
+        {
+            String fname = "Vikas";
+            String lname = "Vats";
+            int age = 23;
+            float height = 5.8f;
+            
+            Console.WriteLine("Here is your personal information");
+            Console.WriteLine("Name : "+fname+""+lname);
+            Console.WriteLine("Age : "+age);
+            Console.WriteLine("Height : "+height);
+
+        }
+    }
+}
